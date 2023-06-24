638ca29b77b51dff7ab3dad9e6102aec98d040fd
RichardYYang <36674468+RichardYYang@users.noreply.github.com>
2023-06-23 20:13:39 -0400

    Adding the Structure for Card

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ window.addEventListener('load', async () => {
     } else {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
+
+    // Create Card Form
+    const createCardForm = document.getElementById('createCardForm');
+    createCardForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        
+        const name = document.getElementById('name').value;
+        const description = document.getElementById('description').value;
+        const price = document.getElementById('price').value;
+        
+        try {
+            await marketplaceContract.methods.createCard(name, description, price).send({ from: web3.eth.defaultAccount });
+            alert('Card created successfully!');
+        } catch (error) {
+            console.error(error);
+            alert('Failed to create card.');
+        }
